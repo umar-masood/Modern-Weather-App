125c6de88c758953b925a00ac7c8ca9e3de2a856
umarmasood1 <umarmasood8546@gmail.com>
2025-07-29 11:54:27 +0500

    Enabled High DPI Scaling

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 int main(int argc, char *argv[])
 {
    QApplication app(argc, argv);
+
+   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+   QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
    QCoreApplication::setApplicationName("Weather");
    QCoreApplication::setOrganizationName("Umar Creations");
    QCoreApplication::setApplicationVersion("2.0.0");
